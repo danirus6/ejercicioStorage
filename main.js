c6a6272d1e3da0927bc6f7114c4e1ae59224e4ba
danirus6 <44194174+danirus6@users.noreply.github.com>
2023-10-20 17:50:11 +0200

    cambios y finalización

--- main.js
+++ main.js
@@ -1,45 +1,48 @@
 const contactForm = document.getElementById('contactForm');
-const nombre = document.getElementById('nombre');
-const correo = document.getElementById('correo');
-const mensaje = document.getElementById('mensaje');
+const contactList = document.getElementById('contactList');
+const borrarContactosButton = document.getElementById('borrarContactos');
 
-const userDisplay = document.getElementById('userDisplay');
-const userName = document.getElementById('userName');
-const userEmail = document.getElementById('userEmail');
-const userMessage = document.getElementById('userMessage');
+let contactosGuardados = [];
+let nombre = '';
+let correo = '';
+let mensaje = '';
 
+// Función para guardar un contacto en Local Storage
+const guardarContacto = () => {
+    const contacto = { nombre, correo, mensaje };
+    contactosGuardados = JSON.parse(localStorage.getItem('contactos')) || [];
+    contactosGuardados.push(contacto);
+    localStorage.setItem('contactos', JSON.stringify(contactosGuardados));
+};
 
-const storedContact = JSON.parse(localStorage.getItem('contact'));
-if (storedContact) {
-    userName.textContent = storedContact.nombre;
-    userEmail.textContent = storedContact.correo;
-    userMessage.textContent = storedContact.mensaje;
-} else {
-    userDisplay.style.display = 'none';
-}
+// Función para mostrar todos los contactos en el DOM
+const mostrarContactos = () => {
+    contactList.innerHTML = '';
+    for (const contacto of contactosGuardados) {
+        const li = document.createElement('li');
+        li.textContent = `Nombre: ${contacto.nombre}, Correo: ${contacto.correo}, Mensaje: ${contacto.mensaje}`;
+        contactList.appendChild(li);
+    }
+};
 
-contactForm.addEventListener("submit", e => {
-    e.preventDefault();
-
-    localStorage['nombre'] = nombre.value;
-    localStorage['mensaje'] = mensaje.value;
-    localStorage['correo'] = correo.value;
-    console.log(localStorage);
-
-    const contactData = {
-        nombre,
-        correo,
-        mensaje
-    };
-
-    localStorage.setItem('contact', JSON.stringify(contactData));
-
-    userName.textContent = contactData.nombre;
-    userEmail.textContent = contactData.correo;
-    userMessage.textContent = contactData.mensaje;
-
-    console.log(userName.textContent);
-
-    userDisplay.style.display = 'block';
-
+// Evento para guardar un contacto cuando se envía el formulario
+contactForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    nombre = document.getElementById('nombre').value;
+    correo = document.getElementById('correo').value;
+    mensaje = document.getElementById('mensaje').value;
+    guardarContacto();
+    mostrarContactos();
+    // Limpiar el formulario
+    contactForm.reset();
 });
+
+// Evento para borrar todos los contactos
+borrarContactosButton.addEventListener('click', function () {
+    localStorage.removeItem('contactos');
+    contactosGuardados = [];
+    mostrarContactos();
+});
+
+// Mostrar los contactos al cargar la página
+mostrarContactos();
